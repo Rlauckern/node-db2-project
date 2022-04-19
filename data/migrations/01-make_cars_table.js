0c057fdc3c71019fdff74848b954bb42a161c9a4
Rlauckern <r.lauckern@gmail.com>
2022-04-19 20:26:20 +0000

    migration up added

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,15 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id')
+    tbl.string('vin', 13).notNullible().unique()
+    tbl.string('make', 128).notNullible()
+    tbl.string('model', 128).notNullible()
+    tbl.numeric('mileage').notNullible()
+    tbl.string('title', 128)
+    tbl.string('transmission', 128)
+    
+  })
 };
 
 exports.down = function (knex) {
