597b484120e5cb96b66352e377e92735687fa1b7
Rlauckern <r.lauckern@gmail.com>
2022-04-19 20:19:35 +0000

    seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,24 @@
 // STRETCH
+const cars = [
+    {
+    vin: '1111111111111',
+    make: "toyota",
+    model: "prius",
+    mieage: 215000,
+    title: "clean",
+    transmission: "manual",
+    },
+    {
+     vin: '1111111111111',
+     make: "toyota",
+     model: "prius",
+     mileage: 215000,
+     title: "clean",
+     transmission: "manual",
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
